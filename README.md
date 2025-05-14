ac6d7e934f07a1079952309a1f427399b9737228
mininxd <mininsna@gmail.com>
2025-05-14 04:54:49 +0000

    idk what im doing

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 check `.env-example` and put your API URL from VPS
 
 __ORIGIN_URL : "your VPS API"__
+__CLAN_TAG : "your clan tag"__
 
 Example :
 
 __ORIGIN_URL : "192.168.1.1:3000"__
+__CLAN_TAG : "#123456"__
--- index.js
+++ index.js
@@ -4,31 +4,50 @@ import express from "express";
 
 const app = express()
 
-
 app.get('/', async (req, res) => {
-  let {data} = await axios.get(`${process.env.ORIGIN_URL}`)
+  const tag = req.query.tag;
+  const clanTag = encodeURIComponent(tag || process.env.CLAN_TAG);
+
+ try {
+   let {data} = await axios.get(`${process.env.ORIGIN_URL}/${clanTag}`)
   res.send(data)
-})
+  } catch(e) {
+    console.log(e)
+    res.send(e.message)
+  }
+  })
 
 app.get('/war', async (req, res) => {
+  const tag = req.query.tag;
+  const clanTag = encodeURIComponent(tag || process.env.CLAN_TAG);
   try {
-  let {data} = await axios.get(`${process.env.ORIGIN_URL}/war`)
+  let {data} = await axios.get(`${process.env.ORIGIN_URL}/war/${clanTag}`)
   res.send(data)
-  } catch(e) {res.send(e.message)}
+  } catch(e) {
+    console.log(e)
+    res.send(e.message)}
 })
+
 app.get('/war/log', async (req, res) => {
+  const tag = req.query.tag;
+  const clanTag = encodeURIComponent(tag || process.env.CLAN_TAG);
+  
   try {
-  let {data} = await axios.get(`${process.env.ORIGIN_URL}/war/log`)
+  let {data} = await axios.get(`${process.env.ORIGIN_URL}/war/log/${clanTag}`)
   res.send(data)
-  } catch(e) {res.send(e.message)}
+  } catch(e) {
+    console.log(e)
+    res.send(e.message)}
 })
+
+/*
 app.get('/war/liga', async (req, res) => {
   try {
   let {data} = await axios.get(`${process.env.ORIGIN_URL}/war/league`)
   res.send(data)
   } catch(e) {res.send(e)}
 })
-
+*/
 
 app.listen(3000, () => {
   console.log(`localhost:3000`);
